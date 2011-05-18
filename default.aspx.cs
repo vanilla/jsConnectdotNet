57ebe4e782186c9aeceb49bb31aa0a36ac6ae08c
Todd Burry <todd@vanillaforums.com>
2011-05-18 17:36:47 -0400

    Updated the example page to be more clear.

--- default.aspx.cs
+++ default.aspx.cs
@@ -6,29 +6,49 @@ using System.Collections;
 using Vanilla;
 
 /// <summary>
-/// Customize this page
+/// This is an example page for using Vanilla's jsConnect library.
+/// Please note that this pagee must serve its output in a special format.
+/// Do not include your own calls to Response.Write() or else you may encounter unexpected behaviour.
 /// </summary>
 public partial class _Default : System.Web.UI.Page {
     protected void Page_Init(object sender, EventArgs e) {
+    	try {
 		// 1. Get your client ID and secret here. These must match those in your jsConnect settings.
 		string clientID = "123";
 		string secret = "123";
 
 		// 2. Grab the current user from your session management system or database here.
 
+		bool signedIn = true; // this is just a placeholder
+		
 		// YOUR CODE HERE.
 
 		// 3. Fill in the user information in a way that Vanilla can understand.
 		SortedList user = new SortedList();
-		// CHANGE THESE FOUR LINES.
-		user["uniqueid"] = 1;
-		user["name"] = "JohnDoe";
-		user["email"] = "john.doe@anonymous.com";
-		user["photourl"] = "";
+		
+		if (signedIn) {
+			// CHANGE THESE FOUR LINES.
+			user["uniqueid"] = 1;
+			user["name"] = "JohnDoe";
+			user["email"] = "john.doe@anonymous.com";
+			user["photourl"] = "";
+		}
 
 		// 4. Generate the jsConnect string.
-		string js = jsConnect.GetJsConnectString(user, Request.QueryString, clientID, secret, false);
+		bool secure = false; // this should be true unless you are testing
+		string js = jsConnect.GetJsConnectString(user, Request.QueryString, clientID, secret, secure);
+		
 		Response.Write(js);
 		Response.End();
+		
+		} catch (Exception ex) {
+			SortedList exCollection = new SortedList();
+			exCollection['error'] = 'exception';
+			exCollection['message'] = ex.Message;
+			
+			string exString = jsConnect.JsonEncode(exCollection);
+			Response.Write(exString);
+			Response.End();
+		}
     }
 }
