d1acab3bbdc3c5304c7276ea5ef404892ccfe65b
Todd Burry <todd@vanillaforums.com>
2012-03-20 12:12:39 -0400

    Use DateTime.UtcNow instead of DateTime.Now for people on non-utc servers.

--- App_Code/jsConnect.cs
+++ App_Code/jsConnect.cs
@@ -155,7 +155,7 @@ namespace Vanilla {
 			HttpResponse response = HttpContext.Current.Response;
 
 			DateTime epoch = new DateTime(1970, 1, 1);
-			TimeSpan ts = new TimeSpan(DateTime.Now.Ticks - epoch.Ticks);
+			TimeSpan ts = new TimeSpan(DateTime.UtcNow.Ticks - epoch.Ticks);
 			return Convert.ToInt32(ts.TotalSeconds);
 		}
 	}
