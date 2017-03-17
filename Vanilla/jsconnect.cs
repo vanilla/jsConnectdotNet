aaefc344dc843f512bf5a1cd40aeedb91a084d83
DaazKu <daazku@gmail.com>
2017-03-17 14:05:06 -0400

    Update jsConnect to 2.0

--- Vanilla/jsconnect.cs
+++ Vanilla/jsconnect.cs
@@ -1,196 +1,207 @@
 using System;
-using System.Data;
-using System.Configuration;
 using System.Web;
-using System.Web.Security;
 using System.Collections;
 using System.Text;
 using System.Collections.Specialized;
 
 namespace Vanilla {
-	/// <summary>
-	/// This object contains the client code for Vanilla jsConnect signle-sign-on.
-	/// Version 1.0.3
-	/// </summary>
-	public class jsConnect {
-		public static bool Debug = false;
+    /// <summary>
+    /// This object contains the client code for Vanilla jsConnect signle-sign-on.
+    /// Version 2.0
+    /// </summary>
+    public class jsConnect {
+        public const string Version = "2";
+        public const int Timeout = 24 * 60;
 
-		protected static IDictionary Error(string code, string message) {
-			IDictionary result = new SortedList();
-			result["error"] = code;
-			result["message"] = message;
-			
-			return result;
-		}
-		
-		public static string GetJsConnectString(IDictionary user, NameValueCollection request, string clientID, string secret, bool secure, string hash) {
-			if(request == null)
-				request = System.Web.HttpContext.Current.Request.QueryString;
-			
-			IDictionary error = null;
-			
-			int timestamp = 0;
-			try {
-				timestamp = System.Convert.ToInt32(request["timestamp"]);
-			} catch {
-				timestamp = 0;
-			}
-			int currentTimestamp = jsConnect.Timestamp();
-			
-			if (secure) {
-				if(request["client_id"] == null)
-					error = jsConnect.Error("invalid_request", "The client_id parameter is missing.");
-				else if(request["client_id"] != clientID)
-					error = jsConnect.Error("invalid_client", "Unknown client " + request["client_id"] + ".");
-				else if(request["timestamp"] == null && request["signature"] == null) {
-					if(user != null && user.Count > 0) {
-						error = new SortedList();
-						error["name"] = user["name"];
-						error["photourl"] = user.Contains("photourl") ? user["photourl"] : "";
-						error["signedin"] = true;
-					} else {
-						error = new SortedList();
-						error["name"] = "";
-						error["photourl"] = "";
-					}
-				} else if(timestamp == 0) {
-					error = jsConnect.Error("invalid_request", "The timestamp is missing or invalid.");
-				} else if(request["signature"] == null)
-					error = jsConnect.Error("invalid_request", "The signature is missing.");
-				else if(Math.Abs(currentTimestamp - timestamp) > 30 * 60)
-					error = jsConnect.Error("invalid_request", "The timestamp is invalid.");
-				else {
-					// Make sure the timestamp's signature checks out.
-					string timestampSig = jsConnect.Hash(timestamp.ToString() + secret, hash);
-					if(timestampSig != request["signature"])
-						error = jsConnect.Error("access_denied", "Signature invalid.");
-				}
-			}
+        public static bool Debug = false;
 
-			IDictionary result;
-			
-			if(error != null)
-				result = error;
-			else if(user != null && user.Count > 0) {
-				result = new SortedList(user);
-				SignJsConnect(result, clientID, secret, true, hash);
-			} else {
-				result = new SortedList();
-				result["name"] = "";
-				result["photourl"] = "";
-			}
-			
-			string json = jsConnect.JsonEncode(result);
-			if(request["callback"] == null)
-				return json;
-			else
-				return request["callback"] + "(" + json + ")";
-		}
+        protected static IDictionary Error(string code, string message) {
+            IDictionary result = new SortedList();
+            result["error"] = code;
+            result["message"] = message;
+            
+            return result;
+        }
+        
+        public static string GetJsConnectString(IDictionary user, NameValueCollection request, string clientID, string secret, bool secure, string hash) {
+            if(request == null)
+                request = System.Web.HttpContext.Current.Request.QueryString;
+            
+            IDictionary error = null;
+            
+            int timestamp = 0;
+            try {
+                timestamp = System.Convert.ToInt32(request["timestamp"]);
+            } catch {
+                timestamp = 0;
+            }
+            int currentTimestamp = jsConnect.Timestamp();
+            
+            if (secure) {
+                if (request["v"] == null)
+                    error = jsConnect.Error("invalid_request", "Missing the v parameter.");
+                else if (request["v"] != jsConnect.Version)
+                    error = jsConnect.Error("invalid_request", "Unsupported version " + request["v"] + ".");
+                else if(request["client_id"] == null)
+                    error = jsConnect.Error("invalid_request", "Missing the client_id parameter.");
+                else if(request["client_id"] != clientID)
+                    error = jsConnect.Error("invalid_client", "Unknown client " + request["client_id"] + ".");
+                else if(request["timestamp"] == null && request["signature"] == null) {
+                    if(user != null && user.Count > 0) {
+                        error = new SortedList();
+                        error["name"] = user["name"];
+                        error["photourl"] = user.Contains("photourl") ? user["photourl"] : "";
+                        error["signedin"] = true;
+                    } else {
+                        error = new SortedList();
+                        error["name"] = "";
+                        error["photourl"] = "";
+                    }
+                } else if(timestamp == 0) {
+                    error = jsConnect.Error("invalid_request", "The timestamp is missing or invalid.");
+                } else if(request["sig"] == null)
+                    error = jsConnect.Error("invalid_request", "Missing the sig paremeter.");
+                else if(Math.Abs(currentTimestamp - timestamp) > jsConnect.Timeout)
+                    error = jsConnect.Error("invalid_request", "The timestamp is invalid.");
+                else if (request["nonce"] == null)
+                    error = jsConnect.Error("invalid_request", "Missing the nonce parameter.");
+                else if (request["ip"] == null)
+                    error = jsConnect.Error("invalid_request", "Missing the ip parameter.");
+                else {
+                    // Make sure the signature checks out.
+                    string sig = jsConnect.Hash(request["ip"] + request["nonce"] + timestamp.ToString() + secret, hash);
+                    if(sig != request["sig"])
+                        error = jsConnect.Error("access_denied", "Signature invalid.");
+                }
+            }
 
-		/// <summary>
-		/// Backwards compatability version of GetJsConnectString().
-		/// </summary>
-		public static string GetJsConnectString(IDictionary user, NameValueCollection request, string clientID, string secret, bool secure) {
-			return GetJsConnectString (user, request, clientID, secret, secure, "md5");
-		}
-		
-		/// <summary>
-		/// Encode a dictionary as json.
-		/// </summary>
-		/// <param name="d">The data to encode.</param>
-		/// <returns>The json encoded string.</returns>
-		public static string JsonEncode(IDictionary data) {
-			var json = new System.Web.Script.Serialization.JavaScriptSerializer();
+            IDictionary result;
+            
+            if(error != null)
+                result = error;
+            else if(user != null && user.Count > 0) {
+                user.Add("ip", request["ip"]);
+                user.Add("nonce", request["nonce"]);
+                result = new SortedList(user);
+                SignJsConnect(result, clientID, secret, true, hash);
+                result.Add("v", jsConnect.Version);
+            } else {
+                result = new SortedList();
+                result["name"] = "";
+                result["photourl"] = "";
+            }
+            
+            string json = jsConnect.JsonEncode(result);
+            if(request["callback"] == null)
+                return json;
+            else
+                return request["callback"] + "(" + json + ")";
+        }
 
-			string result = json.Serialize(data);
-			return result;
-		}
+        /// <summary>
+        /// Backwards compatability version of GetJsConnectString().
+        /// </summary>
+        public static string GetJsConnectString(IDictionary user, NameValueCollection request, string clientID, string secret, bool secure) {
+            return GetJsConnectString (user, request, clientID, secret, secure, "md5");
+        }
+        
+        /// <summary>
+        /// Encode a dictionary as json.
+        /// </summary>
+        /// <param name="d">The data to encode.</param>
+        /// <returns>The json encoded string.</returns>
+        public static string JsonEncode(IDictionary data) {
+            var json = new System.Web.Script.Serialization.JavaScriptSerializer();
 
-		public static string Hash(string password, string method) {
-			byte[] textBytes = System.Text.Encoding.Default.GetBytes(password);
-			try {
-				System.Security.Cryptography.HashAlgorithm cryptHandler = null;
+            string result = json.Serialize(data);
+            return result;
+        }
 
-				switch (method.ToLower()) {
-				case "sha1":
-					cryptHandler = System.Security.Cryptography.SHA1.Create();
-					break;
-				case "sha256":
-					cryptHandler = System.Security.Cryptography.SHA256.Create();
-					break;
-				case "":
-				case "md5":
-				default:
-					cryptHandler = System.Security.Cryptography.MD5.Create();
-					break;
-				}
+        public static string Hash(string password, string method) {
+            byte[] textBytes = System.Text.Encoding.Default.GetBytes(password);
+            try {
+                System.Security.Cryptography.HashAlgorithm cryptHandler = null;
 
-				byte[] hash = cryptHandler.ComputeHash(textBytes);
-				string ret = "";
-				foreach(byte a in hash) {
-					if(a < 16)
-						ret += "0" + a.ToString("x");
-					else
-						ret += a.ToString("x");
-				}
-				return ret;
-			} catch {
-				throw;
-			}
-		}
-		
-		public static string StrUpper(System.Text.RegularExpressions.Match m) {
-			return m.ToString().ToUpper();
-		}
-		
-		public static string SignJsConnect(IDictionary data, string clientID, string secret, bool setData, string hash) {
-			// Generate a sorted list of the keys.
-			string[] keys = new string[data.Count];
-			data.Keys.CopyTo(keys, 0);
-			Array.Sort(keys, new CaseInsensitiveComparer());
-			
-			// Generate the string to sign.
-			StringBuilder sigStr = new StringBuilder();
-			foreach(string key in keys) {
-				if(sigStr.Length > 0)
-					sigStr.Append("&");
-				
-				string encValue = jsConnect.UrlEncode(data[key] == null ? "" : data[key].ToString());
-				
-				sigStr.AppendFormat("{0}={1}", jsConnect.UrlEncode(key.ToLower()), encValue);
-			}
-			
-			// Sign the string with the secret.
-			string signature = jsConnect.Hash(sigStr.ToString() + secret, hash);
-			
-			if(setData) {
-				data["clientid"] = clientID;
-				data["signature"] = signature;
+                switch (method.ToLower()) {
+                case "sha1":
+                    cryptHandler = System.Security.Cryptography.SHA1.Create();
+                    break;
+                case "sha256":
+                    cryptHandler = System.Security.Cryptography.SHA256.Create();
+                    break;
+                case "":
+                case "md5":
+                default:
+                    cryptHandler = System.Security.Cryptography.MD5.Create();
+                    break;
+                }
 
-				if (Debug)
-					data["sigStr"] = sigStr.ToString();
-			}
-			return signature;
-		}
-		
-		public static int Timestamp() {
-			DateTime epoch = new DateTime(1970, 1, 1);
-			TimeSpan span = (DateTime.UtcNow - epoch);
-			return (int)span.TotalSeconds;
-		}
+                byte[] hash = cryptHandler.ComputeHash(textBytes);
+                string ret = "";
+                foreach(byte a in hash) {
+                    if(a < 16)
+                        ret += "0" + a.ToString("x");
+                    else
+                        ret += a.ToString("x");
+                }
+                return ret;
+            } catch {
+                throw;
+            }
+        }
+        
+        public static string StrUpper(System.Text.RegularExpressions.Match m) {
+            return m.ToString().ToUpper();
+        }
+        
+        public static string SignJsConnect(IDictionary data, string clientID, string secret, bool setData, string hash) {
+            // Generate a sorted list of the keys.
+            string[] keys = new string[data.Count];
+            data.Keys.CopyTo(keys, 0);
+            Array.Sort(keys, new CaseInsensitiveComparer());
+            
+            // Generate the string to sign.
+            StringBuilder sigStr = new StringBuilder();
+            foreach(string key in keys) {
+                if(sigStr.Length > 0)
+                    sigStr.Append("&");
+                
+                string encValue = jsConnect.UrlEncode(data[key] == null ? "" : data[key].ToString());
+                
+                sigStr.AppendFormat("{0}={1}", jsConnect.UrlEncode(key.ToLower()), encValue);
+            }
+            
+            // Sign the string with the secret.
+            string signature = jsConnect.Hash(sigStr.ToString() + secret, hash);
+            
+            if(setData) {
+                data["clientid"] = clientID;
+                data["sig"] = signature;
 
-		public static string UrlEncode(string s) {
-			System.Text.RegularExpressions.MatchEvaluator me = new System.Text.RegularExpressions.MatchEvaluator(jsConnect.StrUpper);
+                if (Debug)
+                    data["sigStr"] = sigStr.ToString();
+            }
+            return signature;
+        }
+        
+        public static int Timestamp() {
+            DateTime epoch = new DateTime(1970, 1, 1);
+            TimeSpan span = (DateTime.UtcNow - epoch);
+            return (int)span.TotalSeconds;
+        }
 
-			string result = HttpUtility.UrlEncode(s);
-			result = System.Text.RegularExpressions.Regex.Replace(result, "%[0-9a-f][0-9a-f]", me);
-			result = result.Replace("'", "%27");
-			result = result.Replace("!", "%21");
-			result = result.Replace("*", "%2A");
-			result = result.Replace("(", "%28");
-			result = result.Replace(")", "%29");
+        public static string UrlEncode(string s) {
+            System.Text.RegularExpressions.MatchEvaluator me = new System.Text.RegularExpressions.MatchEvaluator(jsConnect.StrUpper);
 
-			return result;
-		}
-	}
+            string result = HttpUtility.UrlEncode(s);
+            result = System.Text.RegularExpressions.Regex.Replace(result, "%[0-9a-f][0-9a-f]", me);
+            result = result.Replace("'", "%27");
+            result = result.Replace("!", "%21");
+            result = result.Replace("*", "%2A");
+            result = result.Replace("(", "%28");
+            result = result.Replace(")", "%29");
+
+            return result;
+        }
+    }
 }
